f36418c999af797e9b86cee7c4e5a824abf4010e
pglennonau <pglennon.au@gmail.com>
2026-09-16 03:32:20 +1000

    Add isolated v027h test config

---
+++ test-v027h/config.js
@@ -0,0 +1,11 @@
+window.PERS_CONFIG = {
+  version: "0.27.8",
+  deploymentId: "pers-favourites-v027h-test",
+  mode: "local",
+  appName: "Pers Favourites",
+  ownerDisplayName: "Per",
+  homeRegion: "Melbourne / Travel",
+  supabaseUrl: "",
+  supabasePublishableKey: "",
+  placesSearchEndpoint: ""
+};
