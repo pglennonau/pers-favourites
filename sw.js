4432ce094ac740486ef40744968740a977897629
pglennonau <pglennon.au@gmail.com>
2026-09-16 06:16:36 +1000

    Use clean 0.27.11 service-worker shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'pers-favourites-v027j-shell-v1';
-const SHELL = ['./','./index.html','./styles.css','./app.js','./config.js','./v027j.js','./branding.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE = 'pers-favourites-0.27.11-shell-v1';
+const SHELL = ['./','./index.html','./styles.css','./app.js','./config.js','./branding.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('message', e => { if(e.data?.type==='SKIP_WAITING') self.skipWaiting(); });
